c63e96177cf84397c91fa6f07e824d77af2b48fd
motoshige murai <tororopop@gmail.com>
2017-06-05 08:56:22 +0000

    “2017年6月5日（ダミーアカウントseed）”

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,10 @@
 
 100.times do |n|
   email = Faker::Internet.email
+  name = Faker::Name.name
   password = "password"
   User.create!(email: email,
+               name: name,
                password: password,
                password_confirmation: password,
                )
