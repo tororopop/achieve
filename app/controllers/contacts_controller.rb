758f95e2e32f556d087a32dcef0052b830c854dc
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-06-09 12:01:03 +0900

    “2017年6月9日（DIVE11）”

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,6 +11,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contacts_params)
     if @contact.save
     redirect_to root_path, notice: "お問い合わせありがとうございました！"
+    NoticeMailer.sendmail_contact(@contact).deliver
     else
       # 入力フォームを再描画します。
       render 'new'
--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,4 +11,11 @@ class NoticeMailer < ApplicationMailer
     mail to: "tororopop@gmail.com",
          subject: '【Achieve】ブログが投稿されました'
   end
+
+  def sendmail_contact(contact)
+        @contact = contact
+
+    mail to:  contact.email,
+         subject: '【Achieve】お問い合わせが完了しました'
+  end
 end
