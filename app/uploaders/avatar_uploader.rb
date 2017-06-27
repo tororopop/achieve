97341f84a23d0678b70017fda5a78931a9aed938
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-06-27 13:26:55 +0900

    2017年6月27日（DIVE14_SNS4）

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,7 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-   Create different versions of your uploaded files:
+  # Create different versions of your uploaded files:
    version :thumb do
      process resize_to_fit: [50, 50]
    end
