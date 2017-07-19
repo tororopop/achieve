3c0411ea76960a61191efc2cbd669b09b8e137ea
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-07-19 23:07:45 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'achieve'
 set :repo_url, 'https://github.com/tororopop/achieve'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, ENV['BRANCH'] || 'master'
+# set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
 set :deploy_to, '/var/www/achieve'
